4e8627df0c30702322fdc8c02efe57e7de06dccb
Canns Coding <CannsCoding@gmail.com>
2023-09-07 17:58:03 +0000

    install projects

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -44,17 +44,33 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
+createPlatform(0, 225, 300, 50)
+createPlatform(300, 230, 200, 50)
+createPlatform(500, 235, 200, 50)
+createPlatform(700, 240, 200, 50)
+createPlatform(900, 245, 200, 50)
+createPlatform(1100, 250, 100, 50)
+createPlatform(1100, 525, 250, 50)
+createPlatform(900, 530, 200, 50)
+createPlatform(700, 535, 200, 50)
+createPlatform(500, 540, 200, 50)
+createPlatform(300, 545, 200, 50)
+createPlatform(200, 550, 100, 50)
+createPlatform(0, 625, 80, 20)
+createPlatform(1320, 390, 80, 20)
+createPlatform(1200, 280, 80, 20)
+createPlatform(1350, 660, 80, 20)
 
-
-    
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+   
+  createCollectable("kennedi", 1100, 655, 0)
+  createCollectable("kennedi", 1360, 325, 0)
+  createCollectable("kennedi", 10, 550, 0)
+  createCollectable("kennedi", -25, -30, 0)
 
     // TODO 3
     // Create cannons
@@ -62,8 +78,19 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-
-
+    createCannon("left", 400, 1500)
+    createCannon("left", 300, 2100)
+    createCannon("right", 520, 1600)
+    createCannon("right", 190, 1700)
+    createCannon("right", 200, 1700)
+    createCannon("right", 210, 1700)
+    createCannon("right", 220, 1700)
+    createCannon("right", 230, 1700)
+    createCannon("right", 240, 1700)
+    createCannon("right", 250, 1700)
+    createCannon("right", 260, 1700)
+    createCannon("right", 270, 1700)
+    createCannon("left", 650, 1700)
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
