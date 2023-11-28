5086750dc2f9e5f714e113a0ecda7276a0f50524
Canns Coding <CannsCoding@gmail.com>
2023-11-28 18:55:40 +0000

    password improvment

---
+++ main/user&pass.js
@@ -0,0 +1,14 @@
+    var username = prompt("What is your username")
+    
+        var password = prompt("What is yor password")
+
+            if(username === "Cann" && password === "Geek"){
+                alert("Welcome Randy")
+            }
+            else if(username === "Dumb" && password === "Idiot"){
+                alert("Hello Austin")
+            }
+            else {
+                alert("Go away!!")
+                window.location.href = "https://cannscoding.github.io/main/index.html"
+            }
