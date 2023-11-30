d65569e65358f3939d9dbe204f9a56eb477ed369
Canns Coding <CannsCoding@gmail.com>
2023-11-30 19:35:04 +0000

    better passwords

--- main/user&pass.js
+++ main/user&pass.js
@@ -4,25 +4,13 @@ var Password = prompt("What is your Password")
 if (username === "Cann" && Password === "Geek") {
     alert("Proceed to game")
 }
-else {
-    alert("try again")
-    window.location = "https://cannscoding.github.io/main/Games.html"
-}
-if (username === "Dumb" && Password === "Idiot") {
+else if (username === "Dumb" && Password === "Idiot") {
     alert("Proceed to game")
 }
-else {
-    alert("try again")
-    window.location = "https://cannscoding.github.io/main/Games.html"
-}
-if (username === "Ty" && Password === "Guy") {
+else if (username === "Ty" && Password === "Guy") {
     alert("Proceed to game")
 }
-else {
-    alert("try again")
-    window.location = "https://cannscoding.github.io/main/Games.html"
-}
-if (username === "Limes" && Password === "Lemons") {
+else if (username === "Limes" && Password === "Lemons") {
     alert("Proceed to game")
 }
 else {
