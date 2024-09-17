c56afc4baee663d0479e7453261069f6731f9837
Canns Coding <CannsCoding@gmail.com>
2024-09-17 16:11:52 +0000

    update to all

--- Login-folder/Login.js
+++ Login-folder/Login.js
@@ -1,49 +1,53 @@
 var User = "no"
 var Pass = "no"
 
+function Clicked2(){
+    window.open("/Login-folder/sign-up.html")
+}
+
 function Clicked() {
     let User = document.getElementById("Username").value
     let Pass = document.getElementById("Password").value
     
     if (User === "Cann" && Pass === "Geek") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Dumb" && Pass === "Idiot") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Ty" && Pass === "Guy") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Limes" && Pass === "Lemons") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "CComfront" && Pass === "10234677") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Swimmer" && Pass === "Boy") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "The" && Pass === "Kid") {
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Bradyen" && Pass === "Sarge2021!!"){
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "Sarth" && Pass === "BigMan24"){
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else if (User === "DOGEATER" && Pass === "ASIANDOGEATER"){
         alert("Proceed to site")
-        window.open("https://cannscoding.github.io/Games/GamesfVygKXGshcaTPvpwHbMj.html")
+        window.open("/Games/GamesfVygKXGshcaTPvpwHbMj.html")
     }
     else {
     alert("try again")
