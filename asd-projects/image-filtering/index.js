ff98da9a2b072976a084445dd1b3f9aa51de64cb
Canns Coding <CannsCoding@gmail.com>
2025-03-13 12:59:35 +0000

    image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -19,11 +19,12 @@ function resetAndRender() {
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
 function applyAndRender() {
-  // Multiple TODOs: Call your apply function(s) here
+  // Apply the filters
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
 
-  
-
-  // do not change the below line of code
+  // Render the image
   render($("#display"), image);
 }
 
@@ -32,18 +33,71 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (var row = 0; row < image.length; row++) {
+    for (var col = 0; col < image[row].length; col++) {
+      // Access the current pixel
+      var rgbString = image[row][col];
+
+      // Convert the string to an array of numbers
+      var rgbNumbers = rgbStringToArray(rgbString);
+
+      // Apply the filter
+      filterFunction(rgbNumbers);
+
+      // Convert the array back to a string
+      rgbString = rgbArrayToString(rgbNumbers);
+
+      // Update the image with the modified pixel
+      image[row][col] = rgbString;
+    }
+  }
+}
 
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  // Get the background color
+  var backgroundRgb = rgbStringToArray(image[0][0]);
 
+  for (var row = 0; row < image.length; row++) {
+    for (var col = 0; col < image[row].length; col++) {
+      // Access the current pixel
+      var rgbString = image[row][col];
+
+      // Convert the string to an array of numbers
+      var rgbNumbers = rgbStringToArray(rgbString);
+
+      // Check if the current pixel is not the background color
+      if (rgbString !== backgroundRgb) {
+        // Apply the filter
+        filterFunction(rgbNumbers);
+      }
+
+      // Convert the array back to a string
+      rgbString = rgbArrayToString(rgbNumbers);
+
+      // Update the image with the modified pixel
+      image[row][col] = rgbString;
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(value) {
+  return value < 0 ? 0 : value > 255 ? 255 : value;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(rgbNumbers) {
+  rgbNumbers[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(rgbNumbers) {
+  rgbNumbers[BLUE] = keepInBounds(rgbNumbers[BLUE] - 50);
+}
 
-
-// CHALLENGE code goes below here
+function increaseGreenByBlue(rgbNumbers) {
+  rgbNumbers[GREEN] = keepInBounds(rgbNumbers[GREEN] + rgbNumbers[BLUE]);
+}
